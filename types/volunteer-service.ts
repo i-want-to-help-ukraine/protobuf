9a5d8ae8c9e32f74be11047f85e11bdf0c5fee19
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-03-17 12:30:23 +0100

    feat: volunteer search

--- types/volunteer-service.ts
+++ types/volunteer-service.ts
@@ -196,8 +196,8 @@ export interface VolunteerContactDto {
 export interface SearchVolunteersDto {
   cityIds: string[];
   activityIds: string[];
-  startCursor: string;
   offset: number;
+  startCursor?: string | undefined;
 }
 
 export const VOLUNTEER_SERVICE_PACKAGE_PACKAGE_NAME = "VolunteerServicePackage";
