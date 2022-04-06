137e15b2326950159f9626c29ac137d42eb68347
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-04-06 12:17:51 +0200

    feat: extend volunteer model

--- types/volunteer-service.ts
+++ types/volunteer-service.ts
@@ -27,6 +27,7 @@ export interface VolunteerDto {
   verificationStatus: string;
   cityIds: string[];
   activityIds: string[];
+  authEmail?: string | undefined;
 }
 
 export interface CreateProfileDto {
@@ -41,6 +42,7 @@ export interface CreateProfileDto {
   social: CreateVolunteerSocialDto[];
   paymentOptions: CreateVolunteerPaymentOptionDto[];
   contacts: CreateVolunteerContactDto[];
+  authEmail?: string | undefined;
 }
 
 export interface UpdateProfileDto {
